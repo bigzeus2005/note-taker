1206ebd74a846adc35883b7aec60957b932c8ab7
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-01-02 17:02:36 -0500

    added post route with uuid

--- server.js
+++ server.js
@@ -3,20 +3,31 @@ const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = process.env.PORT || PORT
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get('/api/notes', (req, res) => {
+  const data = fs.readFileSync('./db/db.json', 'utf8');
+  const notes = JSON.parse(data);
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const data = fs.readFileSync('./db/db.json', 'utf8');
+  const notes = JSON.parse(data);
+  req.body.id = uuidv4();
+  notes.push(req.body);
+  const newNote = JSON.stringify(notes, null, 2);
+  fs.writeFileSync('./db/db.json', newNote);
+  res.json(notes);
 });
 
 
@@ -29,11 +40,8 @@ app.get('*', (req, res) => {
 
 
 
-
-
-
-
-
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));

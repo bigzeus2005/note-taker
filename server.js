6e4415abe4879c1e15bae4c2e16412486ec818cf
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-01-02 16:17:02 -0500

    added file paths for /notes and *

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 
 
